2dedba9dfc2b548cdeb409436c4e2bd64f522dd1
Nukutka <snaffuxgreh@outlook.com>
2019-01-20 21:54:17 +0500

    init

---
+++ Streebog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streebog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hash = Streebog.GetHashCode("210987654321098765432109876543210987654321098765432109876543210", 256);
+            Console.WriteLine(hash);
+        }
+    }
+}
